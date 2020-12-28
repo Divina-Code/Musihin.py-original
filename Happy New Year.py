c2801c591a258fe282bd17c7fbd35097c612c25f
prostoprogerr <egorbogehin@gmail.com>
2020-12-28 23:53:58 +0500

    DJ

--- Happy New Year.py
+++ Happy New Year.py
@@ -4,7 +4,7 @@ from random import randint
 number = int(input())
 while number != 20 :
     y = "    " * randint(1,12)
-    print(y,"  *X   " * number)
+    print(y," *X   *X  *  *    " * number)
     time.sleep(1)
     number += 1
 time.sleep(1)
