3a003b0857ca4bad60c9284097766e231a982ce3
prostoprogerr <egorbogehin@gmail.com>
2020-12-28 17:56:05 +0500

    happyny

--- Happy New Year.py
+++ Happy New Year.py
@@ -1,9 +1,13 @@
+import time
 number = int(input())
 while number != 20 :
     print("*W" * number)
     number += 1
+time.sleep(1)
 print("Вот вам снегопад")
+time.sleep(1)
 print("С новым годом:)")
+
 
 
 
